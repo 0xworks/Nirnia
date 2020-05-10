6af2d33c75eb2191cc685b454923b99d4e6bdf51
freeman40 <jrfwaite@gmail.com>
2020-05-10 18:17:16 +1200

    get Nirnia to at least draw something

--- Nirnia/src/MapLayer.cpp
+++ Nirnia/src/MapLayer.cpp
@@ -5,49 +5,65 @@
 #include "Hazel/Renderer/Renderer2D.h"
 
 #include <GLFW/glfw3.h>
+#include <glm/gtc/type_ptr.hpp>
+#include <imgui.h>
 
 MapLayer::MapLayer()
 : Layer("Map")
+, m_CameraController(1280.0f / 720.0f),
+m_SquareColor({0.2f, 0.3f, 0.8f, 1.0f})
 {}
 
 
 void MapLayer::OnAttach() {
+	HZ_PROFILE_FUNCTION();
+	m_CheckerboardTexture = Hazel::Texture2D::Create("assets/textures/Checkerboard.png");
 }
+
 
 void MapLayer::OnDetach() {
+	HZ_PROFILE_FUNCTION();
 }
 
+
 void MapLayer::OnUpdate(Hazel::Timestep ts) {
+	HZ_PROFILE_FUNCTION();
 
+	// Update
+	m_CameraController.OnUpdate(ts);
 
+	// Render
 	Hazel::Renderer2D::ResetStats();
 	Hazel::Renderer2D::StatsBeginFrame();
+	{
+		HZ_PROFILE_SCOPE("Renderer Prep");
+		Hazel::RenderCommand::SetClearColor({0.1f, 0.1f, 0.1f, 1});
+		Hazel::RenderCommand::Clear();
+	}
 
-	// Update (TODO)
+	{
+		static float rotation = 0.0f;
+		rotation += ts * 50.0f;
 
-	// Animate (TODO)
+		HZ_PROFILE_SCOPE("Renderer Draw");
+		Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
+		Hazel::Renderer2D::DrawRotatedQuad({1.0f, 0.0f}, {0.8f, 0.8f}, -45.0f, {0.8f, 0.2f, 0.3f, 1.0f});
+		Hazel::Renderer2D::DrawQuad({-1.0f, 0.0f}, {0.8f, 0.8f}, {0.8f, 0.2f, 0.3f, 1.0f});
+		Hazel::Renderer2D::DrawQuad({0.5f, -0.5f}, {0.5f, 0.75f}, m_SquareColor);
+		Hazel::Renderer2D::DrawQuad({0.0f, 0.0f, -0.1f}, {20.0f, 20.0f}, m_CheckerboardTexture, 10.0f);
+		Hazel::Renderer2D::DrawRotatedQuad({-2.0f, 0.0f, 0.0f}, {1.0f, 1.0f}, rotation, m_CheckerboardTexture, 20.0f);
+		Hazel::Renderer2D::EndScene();
 
-	// Render (TODO)
-	Hazel::RenderCommand::SetClearColor({0.0f, 0.0f, 0.0f, 1});
-	Hazel::RenderCommand::Clear();
+		Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
+		for (float y = -5.0f; y < 5.0f; y += 0.5f) {
+			for (float x = -5.0f; x < 5.0f; x += 0.5f) {
+				glm::vec4 color = {(x + 5.0f) / 10.0f, 0.4f, (y + 5.0f) / 10.0f, 0.7f};
+				Hazel::Renderer2D::DrawQuad({x, y}, {0.45f, 0.45f}, color);
+			}
+		}
+		Hazel::Renderer2D::EndScene();
+	}
 
-	//Hazel::Renderer2D::BeginScene(/* camera here */);
-
-// 	for (size_t row = minRow; row < maxRow; ++row) {
-// 		for (size_t col = minCol; col < maxCol; ++col) {
-// 			GameObject& object = m_Level.GetGameObject(row, col);
-// 			glm::vec3 position(col - m_ViewPort.GetLeft() + 0.5f, row - m_ViewPort.GetBottom() + 0.5f, 0.0f);
-// 			Hazel::Renderer2D::DrawQuad(position, m_TileSize, m_Tiles[(int)object.GetTile()]);
-// 			if (object.IsPlayer()) {
-// 				m_ViewPort.SetPlayerPosition((float)col, (float)row);
-// 				if ((row == m_Level.GetExitRow()) && (col == m_Level.GetExitCol())) {
-// 					m_WonLevel = true;
-// 				}
-// 			}
-// 		}
-// 	}
-
-	//Hazel::Renderer2D::EndScene();
 	Hazel::Renderer2D::StatsEndFrame();
 
 	auto stats = Hazel::Renderer2D::GetStats();
@@ -56,4 +72,27 @@ void MapLayer::OnUpdate(Hazel::Timestep ts) {
 	char buffer[64];
 	sprintf_s(buffer, 64, "Average frame render time: %8.5f (%5.0f fps)", averageRenderTime, averageFPS);
 	glfwSetWindowTitle((GLFWwindow*)Hazel::Application::Get().GetWindow().GetNativeWindow(), buffer);
+}
+
+
+void MapLayer::OnImGuiRender() {
+	HZ_PROFILE_FUNCTION();
+
+	ImGui::Begin("Settings");
+	ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
+	ImGui::End();
+
+	ImGui::Begin("Stats");
+	auto stats = Hazel::Renderer2D::GetStats();
+	ImGui::Text("Renderer2D Stats:");
+	ImGui::Text("Draw Calls: %d", stats.DrawCalls);
+	ImGui::Text("Quads: %d", stats.QuadCount);
+	ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
+	ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
+	ImGui::End();
+}
+
+
+void MapLayer::OnEvent(Hazel::Event& e) {
+	m_CameraController.OnEvent(e);
 }
